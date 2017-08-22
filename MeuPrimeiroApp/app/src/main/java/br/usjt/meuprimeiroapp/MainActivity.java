5009f3a469dcf91c1760e323363ad5c96d7541a0
solange-carlos <solangeusjt2012@hotmail.com>
2017-08-22 16:53:26 -0300

    ok
    ok

---
+++ MeuPrimeiroApp/app/src/main/java/br/usjt/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,30 @@
+package br.usjt.meuprimeiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    }
+
+
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+    public final static String EXTRA_MESSAGE = "br.usjt.meuprimeiroapp.MESSAGE";
+}
